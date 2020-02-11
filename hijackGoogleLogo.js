7fb2b1ea6a7ee4e629d42148b884288d4e84c732
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-02-11 00:08:51 -0600

    Add files via upload

---
+++ hijackGoogleLogo.js
@@ -0,0 +1,13 @@
+// Step 1  
+let hijackGoogleLogo = function(){
+//Step 2
+let googleLogoEl = document.querySelector("#hplogo");
+
+googleLogoEl.src = "https://www.hackyourfuture.dk/static/logo-dark.svg";
+}
+
+function main(){
+    hijackGoogleLogo();
+}
+
+main();
