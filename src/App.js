968c6011291fcfec50962ab77e545e4e2887cdbc
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-04-26 03:37:26 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PROJECT</h1>
+      <h2>Weather</h2>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
