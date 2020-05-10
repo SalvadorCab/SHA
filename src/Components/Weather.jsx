cb5d30a73e38d46dd50e9b453a19c189d5b5d8f5
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-05-10 11:30:37 -0500

    Update Weather.jsx

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -21,8 +21,7 @@ function Weather(){
     position: ""
    });
 
-  //const URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`;
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=Mexico&appid=ac534896b7970fabdbfd83f3626dee64`;
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`;
   
   let cityInfoToShow = "";        // Displays the city General Info
 
